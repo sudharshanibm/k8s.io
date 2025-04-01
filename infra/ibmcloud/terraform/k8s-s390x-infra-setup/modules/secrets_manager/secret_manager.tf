b05b73761dc9fdce11352dcf8acb4fbc140675a3
Sudharshan Muralidharan <157102595+sudharshanibm@users.noreply.github.com>
2025-04-01 19:33:52 +0530

    K8s s390x Infra Setup

---
+++ infra/ibmcloud/terraform/k8s-s390x-infra-setup/modules/secrets_manager/secret_manager.tf
@@ -0,0 +1,115 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+locals {
+  secrets_manager_region     = "eu-de"
+  secrets_manager_name       = "k8s-s390x-secrets-manager"
+  key                        = var.apikey
+  z_service_cred_secret_name = "k8s-s390x-sm-service-credentials-secret"
+}
+
+# Try to find existing Secrets Manager by name
+data "ibm_resource_instance" "existing_sm" {
+  name     = local.secrets_manager_name
+  location = local.secrets_manager_region
+  service  = "secrets-manager"
+}
+
+# Create Secrets Manager only if not found
+resource "ibm_resource_instance" "secrets_manager" {
+  count             = data.ibm_resource_instance.existing_sm.id != "" ? 0 : 1
+  name              = local.secrets_manager_name
+  resource_group_id = var.resource_group_id
+  service           = "secrets-manager"
+  plan              = "standard"
+  location          = local.secrets_manager_region
+  service_endpoints = "public-and-private"
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
+
+# Get correct Secrets Manager ID (existing or newly created)
+locals {
+  secrets_manager_id = (
+    length(data.ibm_resource_instance.existing_sm) > 0 ?
+    data.ibm_resource_instance.existing_sm.guid :
+    ibm_resource_instance.secrets_manager[0].guid
+  )
+}
+
+
+# Generate RSA key
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "ibm_sm_iam_credentials_configuration" "sm_iam_credentials_configuration_instance" {
+  instance_id = local.secrets_manager_id
+  region      = local.secrets_manager_region
+  name        = "iam_config"
+  api_key     = var.apikey
+}
+resource "ibm_sm_iam_credentials_secret" "janitor_secret" {
+  depends_on    = [ibm_sm_iam_credentials_configuration.sm_iam_credentials_configuration_instance]
+  instance_id   = local.secrets_manager_id
+  description   = "API key used by Boskos deployment ibmcloud-janitor."
+  region        = local.secrets_manager_region
+  name          = "boskos-janitor-api-key"
+  labels        = ["rotate:true"]
+  access_groups = [var.janitor_access_group_id]
+
+  //The time-to-live (TTL) or lease duration of generated secret 14400seconds = 4hrs
+  ttl = "14400"
+}
+
+resource "ibm_sm_iam_credentials_secret" "secret_rotator" {
+  depends_on    = [ibm_sm_iam_credentials_configuration.sm_iam_credentials_configuration_instance]
+  instance_id   = local.secrets_manager_id
+  description   = "API key used by secret-manager(rotator)."
+  region        = local.secrets_manager_region
+  name          = "secret-rotator-api-key"
+  access_groups = [var.secret_rotator_access_group_id]
+
+  //Auto rotate secret after 1day = 24hrs, the minimum value is 1
+  rotation {
+    auto_rotate = true
+    interval    = 1
+    unit        = "day"
+  }
+
+  //The time-to-live (TTL) or lease duration of generated secret 86400seconds = 24hrs
+  ttl = "86400"
+}
+# Arbitrary secrets
+resource "ibm_sm_arbitrary_secret" "z_ssh_private_key" {
+  name        = "zvsi-ssh-private-key"
+  instance_id = local.secrets_manager_id
+  region      = local.secrets_manager_region
+  labels      = ["zvsi-ssh-private-key"]
+  payload     = tls_private_key.private_key.private_key_openssh
+}
+
+resource "ibm_sm_arbitrary_secret" "z_ssh_public_key" {
+  name        = "zvsi-ssh-public-key"
+  instance_id = local.secrets_manager_id
+  region      = local.secrets_manager_region
+  labels      = ["zvsi-ssh-public-key"]
+  payload     = tls_private_key.private_key.public_key_openssh
+}
